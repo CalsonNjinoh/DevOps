88b2c9dce1686496ef7013c60eb81f20926006c3
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2023-11-08 21:46:53 -0500

    Create variables.tf

---
+++ Accounts/Development/variables.tf
@@ -0,0 +1,56 @@
+variable "region" {
+  type        = string
+  description = "Name of the region in which cluster need to be created"
+  default = "ca-central-1"
+}
+
+variable "name" {
+  type        = string
+  description = "Name of environment"
+  default     = "development"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Availability Zones"
+  default     = []
+}
+
+variable "cidr" {
+  type        = string
+  description = "CIDR block for VPC"
+  default     = "0.0.0.0/0"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "Public Subnets"
+  default     = []
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "Private Subnets"
+  default     = []
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags"
+  default     = {}
+}
+
+variable "key_path" {
+  type        = string
+  description = "Path to SSH public key"
+  default     = ""
+}
+variable "centralized_vpc_flow_logs_bucket_arn" {
+  type        = string
+  description = "ARN of the centralized S3 bucket for VPC flow logs"
+}
+variable "price_class" {
+  type        = string
+  description = "The price class for this distribution. One of PriceClass_All, PriceClass_200, PriceClass_100"
+  default     = "PriceClass_200"  
+}
