ea15c4458b2dd08dfc7ddd74eb1e390b3c18d69c
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-03-03 02:45:53 -0500

    Update variables.tf

--- Modules/Auto_scaling/variables.tf
+++ Modules/Auto_scaling/variables.tf
@@ -1,25 +1,71 @@
-variable "subnets" {
-  description = "List of subnets for the ALB"
+variable "asg_name" {
+  description = "Name of the Auto Scaling Group"
+}
+
+variable "min_size" {
+  description = "Minimum number of instances in the ASG"
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in the ASG"
+}
+
+variable "desired_capacity" {
+  description = "Desired capacity of the ASG"
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the ASG"
   type        = list(string)
 }
+
+variable "launch_template_name" {
+  description = "Name of the launch template"
+}
+
+variable "launch_template_description" {
+  description = "Description of the launch template"
+}
+
+variable "image_id" {
+  description = "AMI ID for the instances"
+}
+
+variable "instance_type" {
+  description = "Instance type for the instances"
+}
+
+variable "iam_role_name" {
+  description = "Name of the IAM role for instances"
+}
+
+variable "iam_role_description" {
+  description = "Description of the IAM role for instances"
+}
+
+variable "security_group_id" {
+  description = "ID of the security group for instances"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for instances"
+}
 variable "vpc_id" {
-  description = "VPC ID where Jenkins runs"
-  type        = string
+  description = "VPC ID where the load balancer will be deployed"
 }
-variable "alb_security_group" {
-  description = "Security Group for the ALB"
-  type        = string
+variable "alb_subnets" {
+  description = "List of subnet IDs for the Application Load Balancer"
+  type        = list(string)
 }
-variable "glaretram_ec2_instance_id" {
-  description = "The EC2 instance ID for glaretram"
-  type        = string
+variable "alb_security_groups" {
+  description = "List of security group IDs for the Application Load Balancer"
+  type        = list(string)
 }
-variable "acm_certificate_arn" {
-  description = "The ARN of the ACM certificate for the ALB"
-  type        = string
-  
+variable "ssl_certificate_arn" {
+  description = "ARN of the SSL certificate for HTTPS listener"
 }
-variable "alb_dns_name" {
-  description = "The DNS name of the ALB"
-  type        = string
+variable "create_alb_security_group" {
+  description = "Whether to create a security group for the ALB"
+  type        = bool
+  default     = false
 }
