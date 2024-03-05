794523ec8be8f69ed2e479d03ad218819909c9b0
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-03-05 12:11:29 -0500

    Update output.tf

--- Modules/Auto_scaling/output.tf
+++ Modules/Auto_scaling/output.tf
@@ -2,3 +2,6 @@ output "alb_security_group_id" {
   value = var.create_alb_security_group ? aws_security_group.alb_sg[0].id : ""
   description = "ID of the ALB security group"
 }
+output "asg_name" {
+  value = module.asg.autoscaling_group_name
+}
