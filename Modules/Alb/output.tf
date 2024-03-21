8b61577a559719a807ab991ab6501b0f9cc6cedf
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-03-21 11:24:46 -0400

    Update output.tf

--- Modules/Alb/output.tf
+++ Modules/Alb/output.tf
@@ -6,3 +6,8 @@ output "alb_arn" {
   description = "ARN for the ALB"
   value       = aws_lb.glaretram_alb.arn
 }
+
+output "load_balancer_arn" {
+  description = "The ARN of the load balancer"
+  value       = aws_lb.glaretram_alb.arn
+}
