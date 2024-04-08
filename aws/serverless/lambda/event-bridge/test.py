358ccecf63ea0bcebd132a73a967f78f15975167
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-04-08 12:08:31 -0400

    Create test.py

---
+++ aws/serverless/lambda/event-bridge/test.py
@@ -0,0 +1,16 @@
+{
+  "version": "0",
+  "id": "event-id",
+  "detail-type": "EC2 Instance State-change Notification",
+  "source": "aws.ec2",
+  "account": "891377304437",
+  "time": "2020-04-14T01:59:00Z",
+  "region": "ca-central-1",
+  "resources": [
+    "arn:aws:ec2:ca-central-1:891377304437:instance/i-0821c2b4999954851"
+  ],
+  "detail": {
+    "instance-id": "i-0821c2b4999954851",
+    "state": "running"
+  }
+}
