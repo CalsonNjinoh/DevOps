e05f51c0409d6cbd751a07289b75304c3d2db5d4
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-05-29 11:45:52 -0400

    Create main.tf

---
+++ eks-cluster/modules/cluster_iam_roles/main.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "eks-role" {
+  name = "${var.cluster_name}-eks-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-policy-2" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks-role.name
+}
