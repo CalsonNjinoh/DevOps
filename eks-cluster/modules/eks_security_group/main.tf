f2fb5d07173af78c56bf163e175b9dd860f8fcc3
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-05-29 11:46:43 -0400

    Create main.tf

---
+++ eks-cluster/modules/eks_security_group/main.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "eks-sg" {
+  name        = "${var.cluster_name}-eks-sg"
+  description = "EKS Security group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    self        = true
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow inbound HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "mongo access"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-eks-sg"
+  }
+}
