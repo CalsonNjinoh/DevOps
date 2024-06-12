b97edd81ecf867a9c47657fa08d8ed86bc9631ef
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-06-12 01:22:59 -0400

    Create variables.tf

---
+++ eks-cluster/modules/worker_nodes_security_group/variables.tf
@@ -0,0 +1,15 @@
+variable "cluster_name" {
+    description = "The name of the EKS cluster"
+    type        = string
+    }
+
+variable "vpc_id" {
+    description = "The VPC ID where the security group will be created"
+    type        = string
+    }
+
+variable "eks_cluster_security_group_id" {
+    description = "The ID of the security group for the EKS cluster"
+    type        = string
+    }
+  
