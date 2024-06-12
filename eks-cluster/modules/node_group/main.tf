8a05c788a3ef6c73f4f21217271d0633b96ed8b0
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-06-12 01:18:10 -0400

    Update main.tf

--- eks-cluster/modules/node_group/main.tf
+++ eks-cluster/modules/node_group/main.tf
@@ -1,9 +1,12 @@
+
 resource "aws_eks_node_group" "eks-nodegroup" {
   cluster_name    = var.cluster_name
   node_group_name = "${var.cluster_name}-ng"
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.subnet_ids
   instance_types  = var.instance_types
+  
+ 
 
   scaling_config {
     min_size     = lookup(var.scaling_configuration, "min_size", 3)
@@ -11,16 +14,27 @@ resource "aws_eks_node_group" "eks-nodegroup" {
     max_size     = lookup(var.scaling_configuration, "max_size", 6)
   }
 
+
+
   update_config {
     max_unavailable = lookup(var.scaling_configuration, "max_unavailable", 1)
   }
 
-  remote_access {
+  /*remote_access {
     ec2_ssh_key               = var.ec2_ssh_key_name
     source_security_group_ids = [var.security_group_id]
-  }
+  }*/
+
+  #adding arcitecture to the node group
+  ami_type = "AL2_ARM_64"
 
   tags = merge({
-    Name = var.cluster_name
+    Name = "${var.cluster_name}-worker-nodes"
+    environment = var.environment
+    Role = "worker-node"
   }, var.tags)
+
+  labels = {
+    "role" = "worker-node"
+  }
 }
