43d4c5ad51f0f2356a2ee000124e28796c7519cb
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-06-20 12:52:51 -0400

    Update output.tf

--- eks-cluster/modules/vpc/output.tf
+++ eks-cluster/modules/vpc/output.tf
@@ -1,19 +1,24 @@
 output "vpc_id" {
-  value = local.vpc_id
+  description = "The ID of the VPC"
+  value       = aws_vpc.vpc.id
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.private[*].id
+  description = "The IDs of the public subnets"
+  value       = aws_subnet.public[*].id
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.private[*].id
+  description = "The IDs of the private subnets"
+  value       = aws_subnet.private[*].id
 }
 
 output "vpc_cidr_block" {
-  value= var.vpc_cidr
+  description = "The CIDR block of the VPC"
+  value       = var.vpc_cidr
 }
 
 output "ipv6_cidr_block" {
-  value= aws_vpc.vpc[0].ipv6_cidr_block
+  description = "The IPv6 CIDR block of the VPC"
+  value       = aws_vpc.vpc.ipv6_cidr_block
 }
