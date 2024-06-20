1b43093f0e9f63b4da5a8336b3b193835cd7441f
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-06-20 12:38:20 -0400

    Update main.tf

--- eks-cluster/modules/cluster_iam_roles/main.tf
+++ eks-cluster/modules/cluster_iam_roles/main.tf
@@ -1,20 +1,16 @@
 resource "aws_iam_role" "eks-role" {
   name = "${var.cluster_name}-eks-role"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
       },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "eks-cluster-policy" {
@@ -22,7 +18,12 @@ resource "aws_iam_role_policy_attachment" "eks-cluster-policy" {
   role       = aws_iam_role.eks-role.name
 }
 
-resource "aws_iam_role_policy_attachment" "eks-cluster-policy-2" {
+resource "aws_iam_role_policy_attachment" "eks-service-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-vpc-resource-controller-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.eks-role.name
 }
