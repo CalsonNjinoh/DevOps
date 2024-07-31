cff105ac1c6ffc3badadfab049572bde27a3bb1b
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-07-31 12:51:03 -0400

    Update main.tf

--- WAF/Prod/main.tf
+++ WAF/Prod/main.tf
@@ -1,11 +1,25 @@
 provider "aws" {
-  region = "ca-central-1"
+  region = "us-east-1"
 }
-
 module "waf" {
   source      = "../../modules/WAF"
   name        = var.name
-  description = var.description
+  scope = "REGIONAL"
+  description = "IP_set_for_blacklisted_IPs_in_ca-central-1"
   rules       = var.rules
   alb_arn     = var.alb_arn
+  environment = var.environment
+  regions     = var.regions
+  whitelist_ip_addresses = var.whitelist_ip_addresses
+  blacklist_ip_addresses = var.blacklist_ip_addresses_per_region
+
+  create_whitelist_rule = {
+    for region in var.regions : region => false
+  }
+  create_blacklist_rule = {
+    for region in var.regions : region => false
+  }
+
+
+  blacklist_ip_addresses_per_region = var.blacklist_ip_addresses_per_region
 }
