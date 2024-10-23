32455480b39c346cde47c578d77b57035c810ae6
CalsonNjinoh <123415934+CalsonNjinoh@users.noreply.github.com>
2024-10-23 12:28:09 -0400

    Create output.tf

---
+++ Reporting/modules/iam-policies/output.tf
@@ -0,0 +1,39 @@
+output "secrets_policy" {
+  value = one(aws_iam_policy.secrets_policy[*].arn)
+}
+
+output "backup_policy" {
+  value = one(aws_iam_policy.backup_write_policy[*].arn)
+}
+
+output "chime_policy" {
+  value = one(aws_iam_policy.chime_policy[*].arn)
+}
+
+output "workflow_logs_policy" {
+  value = one(aws_iam_policy.workflow_logs_policy[*].arn)
+}
+
+output "atlas_policy" {
+  value = one(aws_iam_policy.atlas_policy[*].arn)
+}
+
+output "cloudwatch_log_policy" {
+  value = one(aws_iam_policy.cloudwatch_log[*].arn)
+}
+
+output "cloudwatch_agent_policy" {
+  value = one(aws_iam_policy.cloudwatch_agent_policy[*].arn)
+}
+
+output "s3_data_uploads_us_arn" {
+  value = one(aws_iam_policy.s3_data_uploads_us[*].arn)
+}
+
+output "systems_manager_get_parameters_arn" {
+  value = one(aws_iam_policy.systems_manager_get_parameters[*].arn)
+}
+
+output "assume_cross_account_role_policy_arn" {
+  value = one(aws_iam_policy.assume_cross_account_role_policy[*].arn)
+}
